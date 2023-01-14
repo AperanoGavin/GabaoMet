9ae3f87ac3a441d5f144b4dc486a0cf479390a2c
AperanoGavin <audesandrine6@gmail.com>
2023-01-14 13:57:19 +0100

    base of app

---
+++ GabaoMet/GabaoMetApp.swift
@@ -0,0 +1,17 @@
+//
+//  GabaoMetApp.swift
+//  GabaoMet
+//
+//  Created by Gaveen Aperano Mouloungui on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct GabaoMetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
